d0f3f054a5519cabf761117690d15c5e9ff3e6fe
Infinite-eyes <cwm930215@gmail.com>
2021-05-28 17:09:10 +0800

    1.add splash2

---
+++ shortcuts/app/src/main/java/com/demo/shortcuts/ui/Splash2Activity.kt
@@ -0,0 +1,14 @@
+package com.demo.shortcuts.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.shortcuts.R
+
+class Splash2Activity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+    }
+
+}
